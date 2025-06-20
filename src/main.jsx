5643bacead786f8f2a12f1af6e8a0d0d784b503a
Heron Mia <howa.anil@gmail.com>
2025-06-20 20:48:51 +0600

    app

--- src/main.jsx
+++ src/main.jsx
@@ -1,8 +1,8 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import { BrowserRouter, Routes, Route } from "react-router";
-import App from "./app";
 import "./index.css";
+import App from "./App";
 const root = document.getElementById("root");
 
 ReactDOM.createRoot(root).render(
